7c380911d2fc6ae9e3ec94a1db1ff4d1aa1cc26b
Saurav Raj <zsaurav.14@gmail.com>
2026-07-28 16:48:09 +0530

    V1 of index page completed

--- indexjs.js
+++ indexjs.js
@@ -1,3 +1,18 @@
-document.getElementById("submitbtn").addEventListener(click,function(){
+document.getElementById("submitbtn").addEventListener("click", function () {
+
+    let maillogin = document.getElementById("maillogin").value;
+    let contactlogin = document.getElementById("contactlogin").value;
+
+    if (!maillogin.includes("mail.jiit.ac.in")) {
+        alert("Enter valid JIIT email");
+        return;
+    }
+
+    if (contactlogin.length != 10) {
+        alert("Enter valid contact number");
+        return;
+    }
+
     window.location.href = "dashboard.html";
-})
+});
+
